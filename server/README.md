b26d19c7672d6d28231df18c701ab813462409ac
egsir1 <egsir111@gmail.com>
2025-08-25 00:28:12 +0900

    feat: ffmpeg start video processing

---
+++ server/README.md
@@ -0,0 +1,19 @@
+# Video Processing app backend
+
+This is express server for video processing app using ffmpeg
+
+## Prerequisites & Installation
+
+In order to get started with this project the following is required:
+
+### Prerequisites
+
+- **Nodejs (>= 18.0) && npm**
+- **VSCode**
+- **Docker**
+
+### Get Started
+
+- clone this => https://github.com/egsir1/FFmpeg-video-processing-app.git
+- cd server & npm install
+- npm run start
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,32 @@
 import express from 'express';
+import ffmpeg from 'fluent-ffmpeg';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+
 const PORT = process.env.PORT || 3000;
-app.get('/', (req, res) => {
-	res.send('Hello World!');
+app.post('/process-video', (req, res) => {
+	// Get path of the input video file from request body
+	const inputFilePath = req.body.inputFilePath;
+	const outputFilePath = req.body.outputFilePath;
+
+	if (!inputFilePath || !outputFilePath) {
+		return res.status(400).send('Input and output file paths are required');
+	}
+
+	ffmpeg(inputFilePath)
+		.outputOption('-vf', 'scale=-1:360') // 360p resolution
+		.on('end', () => {
+			console.log('Processing finished successfully');
+			res.status(200).send('Processing finished successfully');
+		})
+		.on('error', err => {
+			console.log('An error occurred: ' + err.message);
+			res.status(500).send('An error occurred: ' + err.message);
+		})
+		.save(outputFilePath);
 });
 
 app.listen(PORT, () => {
